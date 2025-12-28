f8972a9295f494357a33ac0eee24f8225070ed37
lohithadamisetti123 <23mh1a4413@acoe.edu.in>
2025-12-28 15:59:12 +0530

    fix: convert all config files to ES6 module syntax
    - Update hardhat.config.js to use import/export
    - Update deploy.js to use ES6 imports
    - Update test file to use ES6 imports
    - All files now compatible with type: module in package.json

--- tests/system.spec.js
+++ tests/system.spec.js
@@ -1,5 +1,5 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+import { expect } from "chai";
+import { ethers } from "hardhat";
 
 describe("SecureVault Authorization System", function () {
   let authManager, vault, signer, user, recipient;
